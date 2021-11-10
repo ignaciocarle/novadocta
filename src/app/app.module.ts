f5cea424d14ad0b96248f3bcb0242e33a6f6d070
ignaciocarle <ignaciocarle@gmail.com>
2021-11-10 00:46:41 -0300

    Header listo, esquema de landing creado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule
