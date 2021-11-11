2c62c6fbd1730759866a27314893103ea4d02764
ignaciocarle <ignaciocarle@gmail.com>
2021-11-11 02:08:02 -0300

    Footer maqueteado y courses listo para empezar

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -12,4 +12,29 @@ export class CoursesComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  arrayCursos = [
+    {
+      id: "0000",
+      nombre: "curso 1",
+      banner: "",
+      estado: "abierto"
+    },
+        {
+      id: "0001",
+      nombre: "curso 2",
+      banner: "",
+      estado: "cerrado"
+    },    {
+      id: "0002",
+      nombre: "curso 3",
+      banner: "",
+      estado: "abierto"
+    },    {
+      id: "0003",
+      nombre: "curso 4",
+      banner: "",
+      estado: "finalizado"
+    },
+  ]
+
 }
